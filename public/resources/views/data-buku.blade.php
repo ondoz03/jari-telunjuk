d5d578da3fe9ef55f14194aa82f02cc7fafa3295
Gilang wahyudi <ondozwahyudi@gmail.com>
2023-10-07 19:07:26 +0700

    [update]

--- public/resources/views/data-buku.blade.php
+++ public/resources/views/data-buku.blade.php
@@ -24,6 +24,17 @@
             {{ Str::title(Str::lower($item->judul)) }}
         </figcaption>
 
-        <p class="text-[#515151]">By {{ $item->penulis }}</p>
+        <p class="text-[#515151]"
+            style=" letter-spacing: normal;
+            color: #151515;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            width: 100%;
+            text-align: left;
+            margin-bottom: 5px;">
+            By {{ $item->penulis }}</p>
     </figure>
 @endforeach
