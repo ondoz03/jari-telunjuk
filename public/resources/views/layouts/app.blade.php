0b7ea3b049a3994085ec127266c0e8d7498521f2
Gilang wahyudi <ondozwahyudi@gmail.com>
2023-10-10 01:52:35 +0700

    [update]

--- public/resources/views/layouts/app.blade.php
+++ public/resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
+    {{-- <meta charset="UTF-8"> --}}
     <meta name="viewport"
         content="width=device-width, initial-scale=1">
     <link href="{{ asset('css/app.css') }}"
