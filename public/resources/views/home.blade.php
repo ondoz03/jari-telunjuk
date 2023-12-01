25b6bdb4ca16e5fb4c6a6704070a19c8297fd517
Gilang <ondozwahyudi@gmail.com>
2023-12-02 05:31:39 +0700

    [update]

--- public/resources/views/home.blade.php
+++ public/resources/views/home.blade.php
@@ -548,6 +548,16 @@
                     </svg>
                 </button>
             </header>
+            <div class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+                <h5 class="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200" id="exampleModalScrollableLabel">
+                    Modal title
+                </h5>
+                <button type="button" class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none" data-te-modal-dismiss="" aria-label="Close">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                </button>
+            </div>
             <section class="block h-[calc(100vh_-_200px)] overflow-y-auto p-4 lg:h-[340px] lg:p-0 astro-J7PV25F6">
                 <!-- <main class="=px-4 overflow-auto !font-source-sans xl:px-8"> -->
                 <div class="flex flex-wrap items-center justify-center gap-3 astro-J7PV25F6">
