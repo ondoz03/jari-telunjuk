3033112ed40f79639fb6138fbe0c87b29fa2272d
Febzar <febrizaryanto@gmail.com>
2023-12-03 17:37:30 +0700

    limit category

--- public/app/Http/Controllers/CategoryController.php
+++ public/app/Http/Controllers/CategoryController.php
@@ -13,19 +13,7 @@ class CategoryController extends Controller
 {
     public function index(Request $request)
     {
-        $data = Kategori::get();
-        // with('buku_limit')
-        // with([
-        //     'buku_limit' => function($query) {
-        //          $query->where('kategori_id', 44);
-        //          $query->orWhere('kategori_id', 45);
-        //          $query->with('detail_buku');
-        //          // $query->take(4)->with('detail_buku');
-        //     }
-        // ])
-        // ->where('id', 120)
-        // ->get();
-        // dd($data->toArray());
+        $data = Kategori::inRandomOrder()->limit(4)->get();
         return view('category.index', compact('data'));
     }
 
