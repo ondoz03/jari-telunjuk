4d78bf578272bb832ab8e2f48ad4934639e68028
Gilang <ondozwahyudi@gmail.com>
2024-01-26 00:23:15 +0700

    [update]

--- public/resources/views/comment-review.blade.php
+++ public/resources/views/comment-review.blade.php
@@ -150,20 +150,20 @@
                             </div>
 
                             <p style="font-size: 12px">
-                                <button type="button" onclick="submitLike({{$item->id}})" class="inline-flex px-3 py-2 text-xs font-medium text-center text-white  bg-[#128C55] rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                <button type="button" onclick="submitLike({{$dataFirst->id}})" class="inline-flex px-3 py-2 text-xs font-medium text-center text-white  bg-[#128C55] rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                                     <svg style="margin-right: 5px" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M14 10.945C13.3836 10.289 12.003 8.63215 11.2034 7.04814C11.1703 6.98257 11.0247 6.98456 10.9937 7.05061C10.5221 8.05496 9.07362 9.92941 8 10.945M11.0333 7.44444C10.9392 9.86549 11 15 12 17" stroke="white" stroke-linecap="round"/>
                                         <path d="M10.5376 22.7916C11.0152 22.7207 22.5795 21.1781 22.0978 10.4211C22.0536 9.43274 21.9303 8.53367 21.7387 7.71865M10.5376 22.7916C16.876 22.3728 20.0969 19.8899 21.5383 16.9142M10.5376 22.7916C9.7707 22.9055 8.97982 22.8964 8.19743 22.7725M21.7387 7.71865C21.4988 6.69828 21.1518 5.80967 20.7188 5.04257M21.7387 7.71865C22.6022 10.1105 23.0542 13.7848 21.5383 16.9142M20.7188 5.04257C17.1684 -1.24629 7.83127 0.632493 4.27577 5.04257C2.88063 6.77451 -0.0433281 11.1668 1.38159 16.6571C2.27481 20.0988 5.17269 22.2936 8.19743 22.7725M20.7188 5.04257C22.0697 6.9404 24.0299 11.3848 22.3541 15.4153M21.5383 16.9142C21.8737 16.4251 22.1428 15.9235 22.3541 15.4153M8.19743 22.7725C12.1971 23.4683 20.6281 22.971 22.3541 15.4153" stroke="white" stroke-opacity="0.3" stroke-linecap="round"/>
                                     </svg>
-                                    Yes  ( {{ count($item->review_like->where('is_status_response', 'like'))}})
+                                    Yes  ( {{ count($dataFirst->review_like->where('is_status_response', 'like'))}})
                                 </button>
-                                <button type="button" onclick="submit_ajax_unlike({{$item->id}})" style="margin-left:5px" class=" inline-flex px-3 py-2 text-xs font-medium text-center text-black bg-gray-200 rounded-lg hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                <button type="button" onclick="submit_ajax_unlike({{$dataFirst->id}})" style="margin-left:5px" class=" inline-flex px-3 py-2 text-xs font-medium text-center text-black bg-gray-200 rounded-lg hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300">
                                     <svg style="margin-right: 5px" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M8 13.055C8.61644 13.711 9.99704 15.3679 10.7966 16.9519C10.8297 17.0174 10.9753 17.0154 11.0063 16.9494C11.4779 15.945 12.9264 14.0706 14 13.055M10.9667 16.5556C11.0608 14.1345 11 9 10 7" stroke="black" stroke-linecap="round"/>
                                         <path d="M10.5376 22.7916C11.0152 22.7207 22.5795 21.1781 22.0978 10.4211C22.0536 9.43274 21.9303 8.53367 21.7387 7.71865M10.5376 22.7916C16.876 22.3728 20.0969 19.8899 21.5383 16.9142M10.5376 22.7916C9.7707 22.9055 8.97982 22.8964 8.19743 22.7725M21.7387 7.71865C21.4988 6.69828 21.1518 5.80967 20.7188 5.04257M21.7387 7.71865C22.6022 10.1105 23.0542 13.7848 21.5383 16.9142M20.7188 5.04257C17.1684 -1.24629 7.83127 0.632493 4.27577 5.04257C2.88063 6.77451 -0.0433281 11.1668 1.38159 16.6571C2.27481 20.0988 5.17269 22.2936 8.19743 22.7725M20.7188 5.04257C22.0697 6.9404 24.0299 11.3848 22.3541 15.4153M21.5383 16.9142C21.8737 16.4251 22.1428 15.9235 22.3541 15.4153M8.19743 22.7725C12.1971 23.4683 20.6281 22.971 22.3541 15.4153" stroke="black" stroke-opacity="0.3" stroke-linecap="round"/>
                                     </svg>
 
-                                    No ( {{ count($item->review_like->where('is_status_response', 'unlike'))}})
+                                    No ( {{ count($dataFirst->review_like->where('is_status_response', 'unlike'))}})
                                 </button>
                             </p>
 
